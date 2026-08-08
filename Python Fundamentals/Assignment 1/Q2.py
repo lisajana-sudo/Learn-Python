bc03dd481602ccc2ba7b22068e9662ea86c1b559
lisajana-sudo <janalisa.tech@gmail.com>
2026-08-09 02:14:49 +0530

    complet question 2

--- Python Fundamentals/Assignment 1/Q2.py
+++ Python Fundamentals/Assignment 1/Q2.py
@@ -1,1 +1,8 @@
 #Take two numbers as input from the user and print their sum, difference, product, and quotient.
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
